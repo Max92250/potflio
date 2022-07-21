ad8584b848c2e97d585711eae794992d4019b312
max <maxrai788@gmail.com>
2022-07-21 07:59:57 +0530

    futher update

--- maxpotflio/src/component/portfolio/Portfolio.jsx
+++ maxpotflio/src/component/portfolio/Portfolio.jsx
@@ -3,7 +3,19 @@ import './Portfolio.css'
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>contact</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+      <div className='container portfolio_container'>
+        <article className='portfolio_item-image'>
+          
+          <h3>This is a portfolio item title</h3>
+          <a href='https://github.com' className='btn'>github</a>
+          <a href='https://dribble.com/Alien_pixels' className='btn btn-primary' target='_blank'>Live Demo</a>
+        </article>
+      </div>
+
+    </section>
   )
 }
 
